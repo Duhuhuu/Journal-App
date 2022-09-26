3aa5c76e7e18de67b4f5dd717734105089486895
Duhuhuu <ignisxmizu@gmail.com>
2022-09-26 15:01:23 -0600

    Routes,login, register and navbar

---
+++ src/journal/layout/JournalLayout.jsx
@@ -0,0 +1,28 @@
+import { Box } from "@mui/system"
+import { NavBar } from "../components";
+
+const drawerWidth = 240;
+
+export const JournalLayout = ({ children }) => {
+  return (
+    
+    <Box sx= {{ display: 'flex'}} >
+        {/* Navbar drawerWidth */}
+        <NavBar drawerWidth={ drawerWidth } />
+
+
+        {/* Sidebar drawerWidth */}
+        <Box 
+            component='main'
+            sx={{ flexGrow: 1, p:3}}
+        >
+            {/* Toolbar */}
+            {children}
+        </Box>
+
+    </Box>
+
+
+
+  )
+}
--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,6 +1,11 @@
 
+import { Typography } from "@mui/material"
+import { JournalLayout } from "../layout/JournalLayout"
+
 export const JournalPage = () => {
   return (
-    <div>JournalPage</div>
+    <JournalLayout>
+    <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae quia distinctio aperiam omnis illo voluptates voluptate reprehenderit voluptatem deleniti, itaque sequi repellat id perferendis architecto ipsa. Quam repudiandae possimus eaque.</Typography>
+    </JournalLayout>
   )
 }
