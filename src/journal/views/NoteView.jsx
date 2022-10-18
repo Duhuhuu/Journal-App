6ab68b639eb9882c9e79397eeba1620ac146f775
Duhuhuu <ignisxmizu@gmail.com>
2022-10-17 18:20:04 -0600

    Subir img

--- src/journal/views/NoteView.jsx
+++ src/journal/views/NoteView.jsx
@@ -1,12 +1,13 @@
-import { SaveOutlined } from "@mui/icons-material"
-import { Button, Grid, TextField, Typography } from "@mui/material"
+import { SaveOutlined, UploadOutlined } from "@mui/icons-material"
+import { Button, Grid, IconButton, TextField, Typography } from "@mui/material"
+import { useRef } from "react"
 import { useEffect } from "react"
 import { useMemo } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import Swal from "sweetalert2"
 import 'sweetalert2/dist/sweetalert2.css'
 import { useForm } from "../../hooks/useForm"
-import { setActiveNote, startSaveNote } from "../../store/journal"
+import { setActiveNote, startSaveNote, startUploadingFiles } from "../../store/journal"
 import { ImageGallery } from "../components"
 
 
@@ -21,6 +22,8 @@ export const NoteView = () => {
 
         return newDate.toUTCString();
     }, [date])
+
+    const fileInputRef = useRef();
 
     useEffect(() => {
      dispatch( setActiveNote(formState) );
@@ -42,6 +45,12 @@ export const NoteView = () => {
         dispatch( startSaveNote() );
     }
 
+    const onFileInputChange = ({target}) => {
+        if (target.files === 0) return;
+        // console.log('Subiendo Archivos');
+        dispatch( startUploadingFiles( target.files ) );
+    }
+
 
   return (
     <Grid 
@@ -58,6 +67,23 @@ export const NoteView = () => {
             </Typography>
         </Grid>
         <Grid item>
+
+            <input 
+                type="file"
+                multiple
+                ref={ fileInputRef }
+                onChange={ onFileInputChange  }
+                style={{ display: 'none' }}
+            />
+
+            <IconButton
+                color="primary"
+                disabled={ isSaving }
+                onClick={ () => fileInputRef.current.click() }
+            >
+                <UploadOutlined />
+            </IconButton>
+
             <Button 
             disabled = { isSaving }
             onClick={onSaveNote}
--- src/store/journal/thunks.js
+++ src/store/journal/thunks.js
@@ -1,7 +1,7 @@
 import { async } from "@firebase/util"
 import { collection, doc, setDoc } from "firebase/firestore/lite";
 import { FirebaseDB } from "../../firebase/config";
-import { loadNotes } from "../../helpers";
+import { fileUpload, loadNotes } from "../../helpers";
 import { addNewEmptyNote, savingNewNote, setActiveNote, setNotes, setSaving, updateNote } from "./journalSlice";
 
 
@@ -44,7 +44,7 @@ export const startLoadingNotes = () => {
 export const startSaveNote = () => {
     return async(dispatch, getState) =>{
         
-        dispatch ( setSaving() )
+        dispatch ( setSaving() );
         const { uid } = getState().auth;
         const { active:note } = getState().journal;
         
@@ -56,4 +56,16 @@ export const startSaveNote = () => {
         dispatch( updateNote( note ) )
 
     }
+}
+
+export const startUploadingFiles = (files = []) =>{
+
+    return async(dispatch) =>{
+        dispatch ( setSaving() );
+        console.log(files);
+
+        await fileUpload(files[0]);
+
+    }
+
 }
